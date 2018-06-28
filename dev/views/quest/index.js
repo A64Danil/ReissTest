8b991a00098ed3e5d17e09ac16ddbe4a4ebe31ab
Danil q9450 <a64danil@mail.ru>
2018-06-28 03:02:16 +0300

    quests show on Quest-pages now

--- dev/views/quest/index.js
+++ dev/views/quest/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
 
-var json = require('../../model/quests.json');
+// var json = require('../../model/quests.json');
+import json from '../../model/quests.json';
 
 console.log(json); // this will show the info it in firebug console
+
+
+const QuestItem = props => <li data-key={props.index}>hey - {props.item.title}</li>;
+const testVar = json;
+
+
+
+const QuestList = (props) => {
+    console.log(json);
+    let questItems = props.map((item, index) =>
+        <div data-key={index} key={index}>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+        </div>
+    );
+
+    return questItems;
+}
 
 export default class Quest extends React.Component {
 
@@ -18,10 +37,13 @@ export default class Quest extends React.Component {
     }
 
     render() {
+
+
         return (
             <div>
                 <h1>Hello Kitty!</h1>
                 <p>Here will be a qustion</p>
+                <div>{QuestList(json)}</div>
             </div>
         );
     }
