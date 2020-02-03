287a58b1d1d108421c3b7bba56ba174aab405e92
Danil q9450 <a64danil@mail.ru>
2020-02-03 20:34:15 +0300

    nano react app

--- dev/store/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { answerReducer } from "./answers";
-import { userReducer } from "./user";
-import { animateReducer } from "./animate";
-/* All REDUCERS COMBINED HERE */
-export default combineReducers({ answerReducer, userReducer, animateReducer });
