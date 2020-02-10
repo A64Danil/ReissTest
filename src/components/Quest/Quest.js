b1a7ccc1d9458fd632c12a66f82a2d1a659b1725
Danil q9450 <a64danil@mail.ru>
2020-02-10 15:15:04 +0300

    add animation testing to Quest component

--- src/components/Quest/Quest.js
+++ src/components/Quest/Quest.js
@@ -1,22 +1,80 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styles from "./Quest.scss";
 
-const Quest = ({questInfo, currentQuestNum}) => {
+
+import Transition from "react-transition-group/Transition";
+import TransitionGroup from "react-transition-group/TransitionGroup";
+import CSSTransition from "react-transition-group/CSSTransition";
+import CSSTransitionGroup from "react-transition-group/CSSTransition";
+
+const Quest = ({questInfo, currentQuestNum, transState}) => {
+
+    // const [showQ, setShowQ] = useState(false);
+
+    const [inMove, setInMove] = useState(true);
+
+
+    const showQuest = () => {
+        let newState = !inMove;
+        setInMove(newState)
+    }
 
     //
+    // useEffect(()=> {
+    //     console.log("Render Quest")
+    //     console.log(showQ)
+    //
+    // })
+
+    useEffect(()=> {
+        console.log("reacted")
+        // setInMove(true);
+        // return setInMove(false)
+
+    }, [currentQuestNum])
+
     // console.log(questInfo)
 
+    const duration = 1300;
+
+    const defaultStyle = {
+        transition: `opacity ${duration}ms ease-in-out`,
+        opacity: 0,
+    }
+
+    const transitionStyles = {
+        entering: { opacity: 0.4 },
+        entered:  { opacity: 1 },
+        exiting:  { opacity: 0 },
+        exited:  { opacity: 0 },
+    };
+
     return (
-        <div className={styles.Quest}>
-            <h3>Вопрос #{currentQuestNum} - {questInfo.title}</h3>
-            <p>{questInfo.description}</p>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-            </ul>
-        </div>
+        <>
+            <button onClick={e => showQuest(e)}>toggle</button>
+            <Transition in={inMove} timeout={duration} appear={true}>
+                {state => (
+                    <div style={{
+                        ...defaultStyle,
+                        ...transitionStyles[state]
+                    }}>
+
+                        <div className={styles.Quest}>
+                            <h3>{state} Вопрос #{currentQuestNum} - {questInfo.title}</h3>
+                            <p>{questInfo.description}</p>
+
+                            <ul>
+                                <li>1</li>
+                                <li>2</li>
+                                <li>3</li>
+                                <li>4</li>
+                            </ul>
+                        </div>
+                    </div>
+                )}
+            </Transition>
+        </>
+
     )
 }
 
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -4,11 +4,19 @@ import styles from "./Slider.scss";
 import Quest from './../Quest/Quest'
 import Button from './../Button/Button'
 
+
+import Transition from "react-transition-group/Transition";
+import TransitionGroup from "react-transition-group/TransitionGroup";
+import CSSTransition from "react-transition-group/CSSTransition";
+import CSSTransitionGroup from "react-transition-group/CSSTransition";
+
 import json from "../../model/quests";
 
 const Slider = () => {
     const [currentQuestNum, setCurrentQuestNum] = useState(1)
     const [questInfo, setQuestInfo] =  useState()
+
+
 
     const handleSliderControl = (direction) => {
         if(direction === "next") {
@@ -22,10 +30,11 @@ const Slider = () => {
     useEffect(()=> {
         console.log("Вопрос " + currentQuestNum);
         let quest = json[currentQuestNum - 1]
-        console.log(quest);
+        // console.log(quest);
         setQuestInfo(quest);
 
     }, [currentQuestNum, setCurrentQuestNum])
+
 
     // useEffect(()=> {
     //     console.log('questInfo', questInfo)
@@ -35,6 +44,8 @@ const Slider = () => {
     return (
         <div className={styles.Slider}>
             <Quest questInfo={questInfo} currentQuestNum={currentQuestNum}/>
+
+
 
             <div className={styles.SliderControl}>
                 <button
@@ -61,6 +72,7 @@ Quest.defaultProps = {
         rdesc: "",
         mdesc: ""
     },
+    test: ""
 
 };
 
