069f71a9b73f7eb7cc8e76c1f4eed5aec246b661
Danil q9450 <a64danil@mail.ru>
2020-03-01 01:40:28 +0300

    add new tasks to readme, add styles to startPage

--- src/pages/startpage/index.js
+++ src/pages/startpage/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import styles from "../../global.scss";
+import {Link} from "react-router-dom";
 
 
 const StartPage = ({location, match}) => {
     return (
-        <h1>Welcome to React Parcel Micro App!</h1>
+        <div className={styles.startScreen}>
+            <h1>Теория мотивации Рисса</h1>
+            <h2>ЧТО ДЕЛАЕТ ВАС СЧАСТ&shy;ЛИВЫМ?</h2>
+            <p>Эти характеристики остаются практически неизменными на протяжении всей вашей жизни</p>
+            <Link to='/quests'  className={styles.startTestBtn}>
+                Начать тест <span>&#62;</span>
+            </Link>
+        </div>
+
     )
 }
 
