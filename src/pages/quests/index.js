e9d4c9c97866ab64ee27ae3f434c83d13a14631f
DanilZen <a64danil@mail.ru>
2020-03-23 18:25:42 +0300

    add redirect if user has not name

--- src/pages/quests/index.js
+++ src/pages/quests/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react'
 
 import Slider from "./../../components/Slider/Slider";
 
+import {StoreContext} from "./../../model/Store";
+
 const Quests = ({history}) => {
+    const store = useContext(StoreContext)
+
+    useEffect(()=> {
+        if (!store.userName) {
+            alert("Для прохождения теста вы должны указать имя");
+            history.push('/name');
+        }
+    })
+
     return (
             <Slider history={history}/>
     )
