17758af73372af2cd2a1a303e0347f5f44bba6c0
DanilZen <a64danil@mail.ru>
2020-05-04 01:29:00 +0300

    small refactoring - move getAllUrlParams from pages to HELPRES

--- src/pages/result/index.js
+++ src/pages/result/index.js
@@ -1,4 +1,5 @@
 import React, {useContext, useEffect, useState} from 'react'
+import {urlInlineParser, getAllUrlParams} from "../../helpers/parsers"
 
 import styles from "../../global.scss";
 
@@ -100,74 +101,6 @@ const Result = ({props}) => {
         setUrlToComparePage(newCompareLink);
     }, [paramsToComparePage, setParamsToComparePage])
 
-    function isNumber(char) {
-        return /\d/.test(char);
-    }
-    function isString(char) {
-        return /\w/.test(char);
-    }
-
-    function getAllUrlParams(url) {
-        // извлекаем строку из URL или объекта window
-        var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-
-        // объект для хранения параметров
-        var obj = {};
-
-        // если есть строка запроса
-        if (queryString) {
-
-            // данные после знака # будут опущены
-            queryString = queryString.split('#')[0];
-
-            // разделяем параметры
-            var arr = queryString.split('&');
-
-            for (var i=0; i<arr.length; i++) {
-                // разделяем параметр на ключ => значение
-                var a = arr[i].split('=');
-
-                // обработка данных вида: list[]=thing1&list[]=thing2
-                var paramNum = undefined;
-                var paramName = a[0].replace(/\[\d*\]/, function(v) {
-                    paramNum = v.slice(1,-1);
-                    return '';
-                });
-
-                // передача значения параметра ('true' если значение не задано)
-                var paramValue = typeof(a[1])==='undefined' ? true : a[1];
-
-                // преобразование регистра
-                paramName = paramName.toLowerCase();
-                paramValue = paramValue.toLowerCase();
-
-                // если ключ параметра уже задан
-                if (obj[paramName]) {
-                    // преобразуем текущее значение в массив
-                    if (typeof obj[paramName] === 'string') {
-                        obj[paramName] = [obj[paramName]];
-                    }
-                    // если не задан индекс...
-                    if (typeof paramNum === 'undefined') {
-                        // помещаем значение в конец массива
-                        obj[paramName].push(paramValue);
-                    }
-                    // если индекс задан...
-                    else {
-                        // размещаем элемент по заданному индексу
-                        obj[paramName][paramNum] = paramValue;
-                    }
-                }
-                // если параметр не задан, делаем это вручную
-                else {
-                    obj[paramName] = paramValue;
-                }
-            }
-        }
-
-        return obj;
-    }
-
     function resultCompare(a, b) {
 
         if (a.valueNum < b.valueNum) {
@@ -179,46 +112,6 @@ const Result = ({props}) => {
         // a должно быть равным b
         return 0;
     }
-
-    function urlInlineParser(urlString) {
-        if (!urlString) return ;
-
-        console.log("urlInlineParser");
-        console.log(urlString);
-
-
-        let newResultArr = urlString.split('');
-        // console.log(newResultArr);
-        let nameBuffer = "";
-        // let parsedResult = [];
-        let parsedResult = {};
-
-        newResultArr.forEach((char) => {
-            let singleResultValue = {};
-            // console.log("char " + char)
-            // console.log("num " + isNumber(char))
-            // console.log("str " + isString(char))
-
-            if (isNumber(char)) {
-                // singleResultValue[nameBuffer] = char;
-                // console.log(singleResultValue)
-                parsedResult[nameBuffer] = char;
-                nameBuffer = "";
-                // parsedResult.push(singleResultValue)
-
-            } else if (isString(char)) {
-                nameBuffer += char;
-                // console.log(nameBuffer)
-            }
-        })
-        // создаём буффер
-        // если встретили букву - добавляем в имя буффероа
-        // если встретили цифру - добавляем в значение объкта буффера, добавлЯем объект в фин результат и чистим буффер
-        return parsedResult;
-
-    }
-    // console.log(finalResultArr)
-    // console.log(urlLink)
 
     const copyToClipboard = (e) => {
         const bufferedText = e.currentTarget.textContent;
