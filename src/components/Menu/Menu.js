3411c62940d04602b2349e0ed1919ad5a256bea4
DanilZen <a64danil@mail.ru>
2020-11-21 20:51:08 +0300

    add test styles to hamburger menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,30 +5,46 @@ import styles from "./Menu.scss";
 
 const Menu = () => {
     return (
-        <div className={styles.Menu}>
-            <ul className={styles.MenuList}>
-                <li>
-                    <NavLink to='/' exact activeClassName={styles.active}>
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/quests' activeClassName={styles.active}>
-                        Тест
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/contacts' activeClassName={styles.active}>
-                        Контакты
-                    </NavLink>
-                </li>
-                <li>
-                    <span>
-                        v0.682
-                    </span>
-                </li>
-            </ul>
+        <>
+            <nav  className={styles.Menu} id="hamnav">
+
+                <label htmlFor="hamburger" className={styles.hamburgerLabel}>&#9776;</label>
+                <input type="checkbox" className={styles.hamburgerCheker} id="hamburger"/>
+
+
+                <div id="hamitems" className={styles.hamburgerItems}>
+                    <a href="index.html">Home</a>
+                    <a href="about.html">About</a>
+                    <a href="tech.html">Tech</a>
+                    <a href="games.html">Games</a>
+                    <a href="reviews.html">Reviews</a>
+                </div>
+            </nav>
+            <div className={styles.MenuOLD}>
+                <ul className={styles.MenuList}>
+                    <li>
+                        <NavLink to='/' exact activeClassName={styles.active}>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/quests' activeClassName={styles.active}>
+                            Тест
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/contacts' activeClassName={styles.active}>
+                            Контакты
+                        </NavLink>
+                    </li>
+                    <li>
+                        <span>
+                            v0.682
+                        </span>
+                    </li>
+                </ul>
         </div>
+        </>
     )
 }
 
