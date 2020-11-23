aa15bfe41ad6b6848a667b110d115b4d2a275cb7
DanilZen <a64danil@mail.ru>
2020-11-23 18:29:41 +0300

    delete old menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -11,40 +11,30 @@ const Menu = () => {
                     <label htmlFor="hamburgerMenuIcon" className={styles.hamburgerLabel}>&#9776;</label>
                     <input type="checkbox" className={styles.hamburgerCheker} id="hamburgerMenuIcon"/>
 
-
-                    <div className={styles.hamburgerItems}>
-                        <a href="index.html">Home</a>
-                        <a href="about.html">About</a>
-                        <a href="tech.html">Tech</a>
-                        <a href="/contacts">Контакты</a>
-                        <a href="reviews.html">Reviews</a>
-                    </div>
-                </div>
-            </nav>
-            <div className={styles.MenuOLD}>
-                <ul className={styles.MenuList}>
-                    <li>
-                        <NavLink to='/' exact activeClassName={styles.active}>
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/quests' activeClassName={styles.active}>
-                            Тест
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/contacts' activeClassName={styles.active}>
-                            Контакты
-                        </NavLink>
-                    </li>
-                    <li>
+                    <ul className={styles.hamburgerItems}>
+                        <li>
+                            <NavLink to='/' exact activeClassName={styles.active}>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/quests' activeClassName={styles.active}>
+                                Тест
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/contacts' activeClassName={styles.active}>
+                                Контакты
+                            </NavLink>
+                        </li>
+                        <li>
                         <span>
                             v0.682
                         </span>
-                    </li>
-                </ul>
-        </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </>
     )
 }
