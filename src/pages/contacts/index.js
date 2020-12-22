27b6279534514572f355621e2563eb8c7a8a4e5c
DanilZen <a64danil@mail.ru>
2020-12-22 23:12:35 +0300

    add new content and styles to contact page

--- src/pages/contacts/index.js
+++ src/pages/contacts/index.js
@@ -2,23 +2,68 @@ import React from 'react';
 
 
 import styles from "../../global.scss";
+import Arrow from "../../assets/svg/arrow_normal";
+import {Link} from "react-router-dom";
 
 // TODO: добаить инфу и верстку
 const Contacts = ({location, match}) => {
+
+
+    const { innerWidth: width, innerHeight: height } = window;
+
+    const textAreaSize = {
+        cols: innerWidth < 480 ? 25 : 50,
+        rows: 6
+    }
+
+    console.log(innerWidth);
+    console.log(textAreaSize.cols);
     return (
         <div className={styles.contactsPage}>
             <div className={styles.contactsPage__Container}>
                 <h1>Контакты разработчиков</h1>
 
-                Идея, механика, тексты, дизайн<br />
-                Анна<br />
-                ann.po.work@gmail.com<br />
-                +7 (917) 571-21-50<br />
-                <br />
-                Разработка на React<br />
-                Даниил<br />
-                a64danil@mail.ru<br />
-                https://github.com/A64Danil/ReissTest<br />
+                <div className={styles.mainContent}>
+                    <div className={styles.contacts}>
+
+                        <p className={styles.authorDescript}>Идея, механика, тексты, дизайн</p>
+                        <p className={styles.authorName}>Анна</p>
+                        <p className={styles.authorLinks}>
+                            ann.po.work@gmail.com<br />
+                            +7 (917) 571-21-50
+                        </p>
+                        <p className={styles.authorDescript}>Разработка на React</p>
+                        <p className={styles.authorName}>Даниил</p>
+                        <p className={styles.authorLinks}>
+                            a64danil@mail.ru<br />
+                            https://github.com/A64Danil/ReissTest
+                        </p>
+                    </div>
+                    <div className={styles.questForm}>
+                        <form action="">
+                            <p className={styles.boldText}>
+                                <strong>
+                                Хотите поделиться впечатлениями?<br/>
+                                Есть комментария/предложения/вопросы?
+                                </strong>
+                            </p>
+
+                            <p className={styles.regularText}>Будем рады обратной связи :)</p>
+
+                            <textarea name="" id="" cols={textAreaSize.cols} rows="6" placeholder="Ваше сообщение"/>
+                            <div className={styles.flex}>
+                                <input type="email" placeholder="Ваш email"/>
+                                <button type="submit" value="Отправить" className={styles.sendBtn}>
+                                    Отправить
+                                    <span>
+                                        <Arrow />
+                                    </span>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
