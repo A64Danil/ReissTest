696629bc24d6189316ea5cc30d7c14205fae9441
DanilZen <a64danil@mail.ru>
2020-12-26 18:09:16 +0300

    add yandes metrika

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ import Menu from "./components/Menu/Menu";
 import StoreProvider from './model/Store'
 import Routes from './routes'
 
+import { YMInitializer } from 'react-yandex-metrika';
+
 import "./global.scss";
 
 const testHistory = createBrowserHistory();
 
 const App = () => {
+    // console.log(console);
+    console.log(YMInitializer);
     return (
         <StoreProvider>
             <div className={"outerWrp"}>
+                <YMInitializer accounts={[61210597]}  options={{webvisor: true}} version="2"/>
                 <BrowserRouter >
                         <Menu/>
                         <Routes history={testHistory}/>
