9ee6f9f3e2101557e6795dba0cfd1eedba29752e
DanilZen <a64danil@mail.ru>
2021-01-10 23:16:31 +0300

    change version name

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -69,7 +69,7 @@ const Menu = () => {
                     <ul className={styles.hamburgerItems}>
                         <li>
                             <NavLink to='/' exact activeClassName={styles.active}>
-                                Главная <span>(v0.83)</span>
+                                Главная <span>(v0.9)</span>
                             </NavLink>
                         </li>
                         <li>
